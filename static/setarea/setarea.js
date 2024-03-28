91a86a994541583ea23bd511ceb1c093d474ad3a
Balazs Horvath <acsipont@gmail.com>
2024-03-28 14:39:07 +0100

    Awoo
    Signed-off-by: Balazs Horvath <acsipont@gmail.com>

--- static/setarea/setarea.js
+++ static/setarea/setarea.js
@@ -56,6 +56,9 @@ const RESIZE_DIRECTIONS = {
  * @param {Event} e - The mousedown event.
  */
 function startResizeArea (e) {
+  // Stop event propagation
+  e.stopPropagation()
+
   const enableAreaResizeCheckbox = document.getElementById('enableAreaResize')
   if (!enableAreaResizeCheckbox.checked) return
 
